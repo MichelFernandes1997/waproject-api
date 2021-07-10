3480c6d592902a2422b6432b0696505cbf11f779
MichelFernandes1997 <michelfsa@live.com>
2021-07-10 16:00:36 -0300

    sending documentation of swagger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,11 +4,17 @@ import exameRoute from './exameRoute'
 
 import laboratorioRoute from './laboratorioRoute'
 
+import swaggerUi from 'swagger-ui-express'
+
+import swaggerDocument from '../documentation/swagger.json'
+
 const routes = Router()
 
 routes.get('/healthcheck', (req: Request, res: Response) => {
     return res.send('Api are running!')
 })
+
+routes.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 routes.use(exameRoute, laboratorioRoute)
 
